79abe7c3a7316362e0addec25170902492b2caee
Siraphob <sirapop_t@hotmail.com>
2024-02-03 15:47:18 +0700

    Server add handler another routes

--- server.js
+++ server.js
@@ -15,10 +15,20 @@ app.use(express.json());
 
 //route
 app.get("/", (req, res) => {
-  res.send("Welcome to my Spotify Clone Backend API");
+  // res.send("Welcome to my Spotify Clone Backend API ").json({ route: "s" });
+  res.json({
+    Hi: "Welcome to my Spotify Clone Backend API",
+    route1: "/login method:POST",
+    route2: "/refresh method:POST",
+  });
 });
 
 app.use("/login", require("./routes/login.js"));
 app.use("/refresh", require("./routes/refresh.js"));
+
+//route not use
+app.all("*", (req, res) => {
+  res.redirect("/");
+});
 
 app.listen(port, () => console.log(`Server start at http://localhost:${port}`));
